e727766cb104015ae6789992904e4682bc6b44c7
a11usya <allusya.08.ac@gmail.com>
2023-02-01 15:39:04 +0100

    myfirstcommit

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.nav');
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+}); 
